b1cb0e1fa54131f8dbe65fa11c90285e91017245
15389664778 <1577928754@qq.com>
2020-01-12 14:15:38 +0800

    log message

---
+++ FieldDeclarations3.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type BadChaincode struct {
+	globalValue string
+}
+
+func (t *BadChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+func (t BadChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	fn, args := stub.GetFunctionAndParameters()
+
+	if fn == "setValue" {
+		t.globalValue = args[0]
+		stub.PutState("key", []byte(t.globalValue))
+		return shim.Success([]byte("success"))
+	} else if fn == "getValue" {
+		value, _ := stub.GetState("key")
+		return shim.Success(value)
+	}
+	return shim.Error("not a valid function")
+}
+
+func main() {
+	if err := shim.Start(new(BadChaincode)); err != nil {
+		fmt.Printf("Error starting BadChaincode chaincode: %s", err)
+	}
+}
